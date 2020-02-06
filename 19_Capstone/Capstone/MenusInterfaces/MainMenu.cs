65d5f3830facd328a723ae58ad9e75b852c982cd
Karina Gentile <karinagentile381@gmail.com>
2020-02-06 12:13:29 -0500

    Added partial UI

--- 19_Capstone/Capstone/MenusInterfaces/MainMenu.cs
+++ 19_Capstone/Capstone/MenusInterfaces/MainMenu.cs
@@ -4,7 +4,43 @@ using System.Text;
 
 namespace Capstone.Products
 {
-    public class MainMenu : IDisplayItems
+    public class MainMenu
     {
+
+        public static void Main(string[] args)
+
+        {
+            string input;
+            bool keepGoing = true;
+            while (keepGoing)
+            { 
+            Console.Clear();
+            Console.WriteLine("1) Display Vending Machine Items");
+            Console.WriteLine("2) Purchase"); // go to PurchaseMenu
+            Console.WriteLine("3) Exit");
+            Console.Write("Please enter selection: ");
+            input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "3":
+                        keepGoing = false;
+                        continue;
+                    case "1":
+                        DisplayItems();
+                        break;
+                    case "2":
+                        GoToPurchaseMenu();
+                        break;
+                    default:
+                        continue;
+                }
+                public static void DisplayItems();
+
     }
+        }
+    }
+
 }
+
+
--- 19_Capstone/Capstone/MenusInterfaces/PurchaseMenu.cs
+++ 19_Capstone/Capstone/MenusInterfaces/PurchaseMenu.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace Capstone.Products
 {
-    public class PurchaseMenu : IDisplayItems
-    {
+    public class PurchaseMenu
+    { //when option (2) selected from MainMenu
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("1) Feed Money");
+            Console.WriteLine("2) Select Product");
+            Console.WriteLine("3) Finish Transaction");
+            Console.WriteLine("/n");
+            Console.WriteLine("/n");
+            Console.WriteLine("Current Money Provided {:c}");
+            Console.Write("Please enter selection: ");
+        }
     }
 }
