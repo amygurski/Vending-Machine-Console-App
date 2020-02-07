4ec19afd3ccf17f965e21bb94e048cefce1265ee
Karina Gentile <karinagentile381@gmail.com>
2020-02-07 11:35:36 -0500

    Feed Money Tests

---
+++ 19_Capstone/CapstoneTests/VendingMachineItemTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone;
+
+namespace CapstoneTests
+{
+    [TestClass]
+    public class VendingMachineItemTests
+    {
+        [TestMethod]
+        public void UpdateQuantity()
+
+        {
+            //Arrange
+            VendingMachine vm = new VendingMachine();
+
+            //Act
+
+
+            //Assert
+            Assert.AreEqual());
+
+        }
+
+    }
+}
--- 19_Capstone/CapstoneTests/VendingMachineTests.cs
+++ 19_Capstone/CapstoneTests/VendingMachineTests.cs
@@ -20,5 +20,22 @@ namespace CapstoneTests
             Assert.AreEqual(true, vm.Inventory.ContainsKey("A1"));
             Assert.AreEqual(false, vm.Inventory.ContainsKey("E3"));
         }
+
+        [TestMethod]
+        public void FeedMoneyTests()
+        {
+            //Arrange
+            VendingMachine vm = new VendingMachine();
+
+            //Act
+            vm.FeedMoney(5);
+
+            //Assert
+            Assert.AreEqual(5.00M, vm.Balance);
+
+        }
+
+        // find a way to test DispenseItem()?
+
     }
 }
